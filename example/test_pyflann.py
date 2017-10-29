eab2702f55fe1e29556efb750a2f2beb883b2125
Benjamin Y Chan <me@vervious.com>
2017-10-29 19:19:34 -0400

    Basic

--- example/test_pyflann.py
+++ example/test_pyflann.py
@@ -6,20 +6,36 @@ dataset = np.array(
      [10, 10, 10, 3, 2],
      [100, 100, 2, 30, 1]
      ])
+moreset = np.array(
+    [[2., 3, 4, 2, 3]
+     ])
 testset = np.array(
     [[1., 1, 1, 1, 1],
      [90, 90, 10, 10, 1]
      ])
+# flann = FLANN()
+# result, dists = flann.nn(
+#     dataset, testset, 2, algorithm="kmeans", branching=32, iterations=7, checks=16)
+# print result
+# print dists
+
 flann = FLANN()
-result, dists = flann.nn(
-    dataset, testset, 2, algorithm="kmeans", branching=32, iterations=7, checks=16)
+# params = flann.build_index(dataset, algorithm="kmeans", branching=32, iterations=7)
+params = flann.build_index(dataset, algorithm="autotuned", target_precision=0.9, log_level = "info")
+# print params
+# result, dists = flann.nn_index(testset, 2, checks=16)
+# print result
+# print dists
+# add more points
+flann.add_points(moreset)
+result, dists = flann.nn_index(testset, 2, checks=16)
 print result
 print dists
 
-dataset = np.random.rand(10000, 128)
-testset = np.random.rand(1000, 128)
-flann = FLANN()
-result, dists = flann.nn(
-    dataset, testset, 5, algorithm="kmeans", branching=32, iterations=7, checks=16)
-print result
-print dists
+# dataset = np.random.rand(10000, 128)
+# testset = np.random.rand(1000, 128)
+# flann = FLANN()
+# result, dists = flann.nn(
+#     dataset, testset, 5, algorithm="kmeans", branching=32, iterations=7, checks=16)
+# print result
+# print dists
